316843dcf63138e959844be5a91bc8bbab60311d
neuro <neuro@csn13.(none)>
2010-11-07 19:45:24 -0500

    Serial port input added.

--- willow/bb.py
+++ willow/bb.py
@@ -1,25 +1,42 @@
 import serial, getopt, sys, urllib2
 
 # Set defaults
-port_name = 0
-url = "http://localhost:8000/put"
+g_port_name = 0
+g_host = "localhost:8000"
+g_tag = "serial"
 
 # Read command line
-switches, _ = getopt.getopt(sys.argv[1:], "p:u:")
+switches, _ = getopt.getopt(sys.argv[1:], "p:h:t:")
 for switch, arg in switches:
   if switch == "-p":
     if arg.isdigit():
-      port_name = int(arg)
+      g_port_name = int(arg)
     else:
-      port_name = arg
-  if switch == "-u":
-    url = arg
+      g_port_name = arg
+  if switch == "-h":
+    g_host = arg
+  if switch == "-t":
+    g_tag = arg
 
 # Open serial port
-port = serial.Serial(port_name)
+port = serial.Serial(g_port_name)
 port.open()
+
+def shout(label, obj):
+  print "%-30s: %s" % (label, obj)
+
+shout("Reading data from port", g_port_name)
+shout("Posting data to host", g_host)
+shout("Using tag", g_tag)
 
 # Relay serial port data to url
 while True:
   byte = port.read(1)
-  urllib2.urlopen(conf.url, byte)
+  msg = '{ "tag": "%s", "byte": "%s" }' % (g_tag, byte)
+  shout("Received ", byte)
+  try:
+    urllib2.urlopen("http://" + g_host + "/put", msg)
+    shout("Posted", msg)
+  except urllib2.URLError:
+    shout("ERROR - no willow on", g_host)
+
